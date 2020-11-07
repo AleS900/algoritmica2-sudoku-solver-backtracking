12932cdb732f8c433c011283dcf71d4475685133
AleS900 <60782047+AleS900@users.noreply.github.com>
2020-11-07 07:31:29 -0400

    BugFixes

--- main.py
+++ main.py
@@ -21,22 +21,29 @@ with open("screen.png", "wb") as fp:
     fp.write(result)
 im = Image.open("screen.png")
 
+
+# TO MOD
 # Coordenadas de inicio del sudoku en el
-# dsipositivo (MOD):
+# dsipositivo
 x, y = 18, 230
 
+# TO MOD
 # Dimensiones de cada casilla:
 dx, dy = 108, 108
+
 
 # Variable que nos permite determinar si se
 # realizará alguna acción en el dispositivo
 impr = 0
+
 
 #       mat: contine el sudoku incompleto escaneado, una matriz 9x9
 # touch mat: matriz que contiene una tupla de coordenadas, es donde
 #            se almacena las coordenadas de cada casilla
 mat, touch_mat = [], []
 
+
+# TO MOD
 # Analizar cada casilla, extraer su valor y asignarle una coordenada
 for j in range(1, 10):
     grid_row, t_row = [], []
@@ -49,6 +56,7 @@ for j in range(1, 10):
         t_row.append(((2 * x + dx) // 2, (2 * y + dy) // 2))
         # En nuestra aplicación cada 3 casilla se presenta una línea más
         # gruesa, entonces:
+        ##
         x += 11 if i % 3 == 0 else 6
         # Extraer el número de cada casilla
         temp = pytesseract.image_to_string(imcrop, config='--psm 10')
@@ -58,6 +66,7 @@ for j in range(1, 10):
     x = xcopy
     # En nuestra aplicación cada 3 filas se presenta una línea más
     # gruesa, entonces:
+    ##
     y += dy + (11 if j % 3 == 0 else 6)
     mat.append(grid_row)
     touch_mat.append(t_row)
@@ -66,18 +75,25 @@ for j in range(1, 10):
 # caso contrario se imprime en pantalla el mensaje que indica que el sudoku
 # no es solucionable y cambia el valor de "impr" a 1 para que ninguna acción
 # sea ralizada en pantalla:
+print_board(mat)
+print("_________________________")
 orig_grid = copy.deepcopy(mat)
 if sudoku_solver(mat):
+    print("Sudoku Resuelto: ")
     print_board(mat)
 else:
     print("No existe solución")
     impr = 1
 
+
+# TO MOD
 # Función que permite al programa detectar las coordenadas de cada casilla y
 # y realizar un "click" sobre las mismas:
 def click(i, j):
     device.shell(f'input touchscreen tap {touch_mat[i][j][0]} {touch_mat[i][j][1]}')
 
+
+# TO MOD
 # Función que permite al programa realizar un "click" encima de cada valor asignable
 # al Sudoku utilizando sus coordenas, en caso de esta la app utilizada en este
 # proyecto los valores del 1 al 5 se encuentran en la misma fila, por tanto:
@@ -85,6 +101,8 @@ def select1to5(n):
     arr = [125, 314, 566, 756, 970]
     device.shell(f'input touchscreen tap {arr[n - 1]} 1342')
 
+
+# TO MOD
 # Función que permite al programa realizar un "click" encima de cada valor asignable
 # al Sudoku utilizando sus coordenas, en caso de esta la app utilizada en este
 # proyecto los valores del 6 al 9 se encuentran en la misma fila, por tanto:
@@ -93,6 +111,7 @@ def select6to9(n):
     device.shell(f'input touchscreen tap {arr[n - 6]} 1545')
 
 
+# TO MOD
 # Actualizar los valores en pantalla,realizando una serie de "clicks" en la misma,
 # comenzando con la casilla y luego con el valor que debe ir en la misma, solamente
 # se seleccionan las casillas que originalmente estaban vacías:
@@ -108,3 +127,4 @@ if impr == 0:
                     select1to5(mat[i][j])
                 else:
                     select6to9(mat[i][j])
+
